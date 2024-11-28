01b19a9def76795b660bd7d2e0f08164ea4f19af
Rahul Chaudhary <145273561+Rahulchaudharyji2@users.noreply.github.com>
2024-11-28 13:22:22 +0530

    Update data-source.js

--- src/data-source.js
+++ src/data-source.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbUrl = process.env.MONGO_URL || 'mongodb+srv://rahulchaudharyji2:rzt5aMdQyyhCb2S2@cluster0.nzxzs3o.mongodb.net/EComm?retryWrites=true&w=majority&appName=Cluster0';
+const dbUrl = 'mongodb+srv://rahulchaudharyji2:rzt5aMdQyyhCb2S2@cluster0.nzxzs3o.mongodb.net/EComm?retryWrites=true&w=majority&appName=Cluster0';
 
 
 class Database{
